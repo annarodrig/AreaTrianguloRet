0f5f4cd1d55e119f7a5d302560320e68dd0e192f
Anna Beatriz <annab_olivecom>
2022-04-08 20:00:37 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Cria váriaveis em ponto flutuante
+double areaTriangulo, Base, altura;
+
+Console.Write("Base:");
+Base = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Altura:");
+altura = Convert.ToDouble(Console.ReadLine());
+
+//Calcula Área do triangulo
+areaTriangulo = altura * Base / 2;
+
+Console.Write($"Área:{areaTriangulo}");
